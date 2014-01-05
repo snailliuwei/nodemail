e4278c3fead1fe9859e572ac60ffdc853580ad7a
snailliuwei <702144188@qq.com>
2014-01-05 21:20:40 +0800

    create

---
+++ app.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var markdown = require('markdown-js');
+var partials = require('express-partials');
+var ejs = require('ejs');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var sign = require('./controllers/sign');
+var config = require('./config').config;
+var routes = require('./routes');
+var appRoot = './';
+
+app.configure('development', function() {
+  // View 默认的根目录为 viewsRoot 的值
+  app.set('views', path.join(appRoot, 'views'));
+  // View 引擎默认处理 html 后缀
+  app.set('views engine', 'html');
+  //  启用 View 缓存（在开发阶段被关闭）
+  app.set('view cache', false);
+  app.engine('html', ejs.renderFile);
+  app.engine('md', function(path, options, fn) {
+    fs.readFile(path, 'utf8', function(err, str) {
+      if (err) return fn(err);
+      str = markdown.parse(str).toString();
+      fn(null, str);
+    });
+  });
+
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: config.sessionSecret}));
+  app.use(sign.authUser);
+  app.use(sign.toobusy);
+  app.use(partials());
+  app.use(express.static(path.join(appRoot, 'public')));
+  // 在 HTML 页面中显示程序传递和抛出的异常
+  app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+});
+
+// set static, dynamic helpers
+// 为 layout.html 绑定数据
+app.locals({
+  config: config,
+  csrf: function(req, res) {
+    return req.session ? req.session._csrf : '';
+  }
+});
+routes(app);
+app.listen(config.port);
+console.log(config.host + ':' + config.port);
